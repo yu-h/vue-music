2acf42d73175d03507464524fd9a7dea627c458b
余昊 <894895714@qq.com>
2018-07-19 19:31:35 +0800

    推荐页面提交

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,50 +1,84 @@
 <template>
-    <div class="recommend">
-        <div class="silder-wrapper">
-            <slider>
-                <div v-for="item in recommends">
-                    <a :href="item.linkUrl">
-                        <img :src="item.picUrl" alt="">
-                    </a>
+    <div class="recommend" ref="recommend">
+        <scroll ref="scroll" class="recommend-content" :data="discList">
+            <div>
+                <div v-if="recommends.length" class="silder-wrapper">
+                    <slider>
+                        <div v-for="item in recommends">
+                            <a :href="item.linkUrl">
+                                <img class="needsclick" @load="loadImage" :src="item.picUrl" alt=""/>
+                            </a>
+                        </div>
+                    </slider>
                 </div>
-            </slider>
-        </div>
-        <div class="recommend-list">
-            <h1 class="list-title">热门歌单</h1>
-            <ul>
-
-            </ul>
-        </div>
+                <div class="recommend-list">
+                    <h1 class="list-title">热门歌单</h1>
+                    <ul>
+                        <!--<li v-for="item in discList" class="item">-->
+                            <!--<div class="icon">-->
+                                <!--<img width="60" height="60" v-lazy="item.imgurl" alt=""/>-->
+                            <!--</div>-->
+                            <!--<div class="text">-->
+                                <!--<h2 class="name" v-html="item.creator.name"></h2>-->
+                                <!--<p class="desc" v-html="item.dissname"></p>-->
+                            <!--</div>-->
+                        <!--</li>-->
+                        <li class="liH"></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="loading-container" v-show="!discList.length">
+                <loading></loading>
+            </div>
+        </scroll>
     </div>
 </template>
 
 <script>
     import Slider from 'base/slider/slider'
-    import {getRecommend} from "api/recommend";
+    import {getRecommend, getDiscList} from "api/recommend";
     import {ERR_OK} from "api/config";
-
+    import Scroll from 'base/scroll/scroll'
+    import Loading from 'base/loading/loading'
 	export default {
 		name: "recommend",
         data (){
 			return{
-				recommends:[]
+				recommends:[],
+                discList:[]
             }
         },
         created(){
-			this._getRecommend()
+			this._getRecommend();
+			this._getDiscList();
         },
         methods:{
 			_getRecommend(){
 				getRecommend().then((res) => {
 					if (res.code === ERR_OK){
-						console.log(res.data.slider);
 						this.recommends = res.data.slider;
 					}
                 })
+            },
+            _getDiscList(){
+				getDiscList().then((res) => {
+					if (res.code === ERR_OK){
+						console.log(res.data);
+						// this.discList = res.data.list;
+					}
+                })
+            },
+            loadImage(){
+				if (!this.checkLoaded) {
+					this.$refs.scroll.refresh();
+					this.checkLoaded = true
+                }
             }
         },
         components:{
-			Slider
+			Slider,
+            Scroll,
+			Loading
         }
 	}
 </script>
@@ -92,7 +126,9 @@
                             margin-bottom: 10px
                             color: $color-text
                         .desc
-                            color: $color-text-d
+                            color: $color-text-
+                .liH
+                    height:800px
             .loading-container
                 position: absolute
                 width: 100%
--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload' //图片懒加载
 
 import 'common/stylus/index.styl'
 
-Vue.config.productionTip = false;
+// Vue.config.productionTip = false;
 fastclick.attach(document.body);
-
+Vue.use(VueLazyload, {
+	loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
